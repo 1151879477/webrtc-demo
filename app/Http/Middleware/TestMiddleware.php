945a842b42fcec49826413b69adf75e5c6165df8
daiyu <1151879477@qq.com>
2019-07-23 14:06:45 +0800

    1

--- app/Http/Middleware/TestMiddleware.php
+++ app/Http/Middleware/TestMiddleware.php
@@ -7,8 +7,14 @@ namespace App\Http\Middleware;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Swoft\Bean\Annotation\Mapping\Bean;
 use Swoft\Http\Server\Contract\MiddlewareInterface;
 
+/**
+ * Class TestMiddleware
+ * @package App\Http\Middleware
+ * @Bean()
+ */
 class TestMiddleware implements MiddlewareInterface
 {
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
