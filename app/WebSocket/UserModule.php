0ae0ad4372ebc7095993a2bfe3e5a0054b7242ef
daiyu <1151879477@qq.com>
2019-07-23 16:09:55 +0800

    1

---
+++ app/WebSocket/UserModule.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\WebSocket;
+
+use Swoft\WebSocket\Server\Annotation\Mapping\WsModule;
+use Swoft\WebSocket\Server\Message\Request;
+
+/**
+ * Class UserModule
+ * @package App\WebSocket
+ * @WsModule(
+ *     '/user'
+ * )
+ */
+class UserModule
+{
+    public function onOpen(Request $request, int $fd)
+    {
+        server()->push($fd, 'user module is open');
+    }
+}
