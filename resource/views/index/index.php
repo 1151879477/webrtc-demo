acac2d3b1f6e42dc1342753beb603a3b18cb64f7
daiyu <1151879477@qq.com>
2019-07-24 08:08:34 +0800

    1

--- resource/views/index/index.php
+++ resource/views/index/index.php
@@ -1,7 +1,37 @@
 <html>
 <body>
-    <p>is work</p>
-</body>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.2.0/socket.io.js"></script>
+    <p>login</p>
 
+    <div>
+        <form id="form">
+            <input type="text" name="username">
+            <input type="text" name="password">
+            <button type="button" id="form-submit">提交</button>
+        </form>
+    </div>
+
+</body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script>
+    $('#form-submit').on('click', function(){
+        $.ajax({
+            url: "/user/login",
+            data: {
+                username: $('input[name=username]').val(),
+                password: $('input[name=password]').val()
+            },
+            type: "post",
+            success: function(result){
+                if(result.code ===  0){
+                    alert('登录成功')
+                }else{
+                    alert('用户名或密码错误')
+                }
+            },
+            error: function(xhr){
+                console.log(xhr);
+            }
+        })
+    })
+</script>
 </html>
