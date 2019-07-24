15fa92e9a9ceb44fae8044b629df6ea134ff78d2
daiyu <1151879477@qq.com>
2019-07-24 11:25:58 +0800

    1

--- app/Listener/Server/ServerStartListener.php
+++ app/Listener/Server/ServerStartListener.php
@@ -5,6 +5,7 @@ namespace App\Listener\Server;
 
 
 use Swoft\Bean\Annotation\Mapping\Bean;
+use Swoft\Event\Annotation\Mapping\Listener;
 use Swoft\Event\EventHandlerInterface;
 use Swoft\Event\EventInterface;
 use Swoft\Redis\Redis;
@@ -13,6 +14,7 @@ use Swoft\Redis\Redis;
  * Class ServerStartListener
  * @package App\Listener\Server
  * @Bean()
+ * @Listener()
  */
 class ServerStartListener implements EventHandlerInterface
 {
