4142fbcb35808c7d84981dae7f470c97da6c49c8
dy <1151879477@qq.com>
2019-07-25 20:38:48 +0800

    1

--- resource/views/webrtc/index.php
+++ resource/views/webrtc/index.php
@@ -198,7 +198,7 @@
         return new RTCPeerConnection({
             iceServers: [     // Information about ICE servers - Use your own!
                 {
-                    urls: "stun:stun.l.google.com:19302",
+                    urls: "stun:stun.l.google.com:19302"
                 }
             ]
         }, mediaConstraints);
