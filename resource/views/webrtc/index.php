0a944983452c277330ce37be99048a5309808963
daiyu <1151879477@qq.com>
2019-07-26 09:27:06 +0800

    1

--- resource/views/webrtc/index.php
+++ resource/views/webrtc/index.php
@@ -196,10 +196,32 @@
 
     function createPeerConnection() {
         var mediaConstraints = {};
+        var string = "stun:stun1.l.google.com:19302\n" +
+            "stun:stun2.l.google.com:19302\n" +
+            "stun:stun3.l.google.com:19302\n" +
+            "stun:stun4.l.google.com:19302\n" +
+            "stun:23.21.150.121\n" +
+            "stun:stun01.sipphone.com\n" +
+            "stun:stun.ekiga.net\n" +
+            "stun:stun.fwdnet.net\n" +
+            "stun:stun.ideasip.com\n" +
+            "stun:stun.iptel.org\n" +
+            "stun:stun.rixtelecom.se\n" +
+            "stun:stun.schlund.de\n" +
+            "stun:stunserver.org\n" +
+            "stun:stun.softjoys.com\n" +
+            "stun:stun.voiparound.com\n" +
+            "stun:stun.voipbuster.com\n" +
+            "stun:stun.voipstunt.com\n" +
+            "stun:stun.voxgratia.org\n" +
+            "stun:stun.xten.com"
+        let arr = string.split("\n")
+        console.log(arr);
         return new RTCPeerConnection({
             iceServers: [
-                {"urls":["stun:stun.l.google.com:19302"]},
-                {"urls":["stun:api2.dai-yu.net:3478"]},
+                // {"urls":["stun:stun.l.google.com:19302"]},
+                // {"urls":["stun:api2.dai-yu.net:3478"]},
+                {"urls": arr}
             ]
         }, mediaConstraints);
     }
