33d6c575744203c93427a258514f5f03a51af269
daiyu <1151879477@qq.com>
2019-07-29 15:34:19 +0800

    1

--- resource/views/webrtc/called.php
+++ resource/views/webrtc/called.php
@@ -96,7 +96,7 @@
             }
         },
         "user.mail": function (data) {
-            console.log(data.subject);
+            console.log(data);
             if (data.subject === "offer") {
                 //offer
                 localClient.setRemoteDescription(new RTCSessionDescription(data.data))
