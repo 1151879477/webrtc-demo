f003b02ef6d89d8f05d6c2307031d9e09dd864db
daiyu <1151879477@qq.com>
2019-07-29 09:53:05 +0800

    1

--- resource/views/webrtc/calling.php
+++ resource/views/webrtc/calling.php
@@ -1,10 +1,20 @@
 <html>
 <head>
     <style></style>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
 </head>
 <body>
+<div class="list-group">
+
+    <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
+    <a href="#" class="list-group-item">Morbi leo risus</a>
+    <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+    <a href="#" class="list-group-item">Vestibulum at eros</a>
+</div>
 
 </body>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/reconnecting-websocket/1.0.0/reconnecting-websocket.min.js"></script>
 <script src="/user.js"></script>
@@ -12,7 +22,6 @@
     let url = '127.0.0.1:9000';
     var ws = new ReconnectingWebSocket('ws://' + url + "/user");
     ws.open();
-
     let userId = getUserId();
 
 
