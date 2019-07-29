9806c86b6012ed37e651cc316503c3f7a9b3f3b7
daiyu <1151879477@qq.com>
2019-07-29 15:44:02 +0800

    1

--- resource/views/webrtc/called.php
+++ resource/views/webrtc/called.php
@@ -131,7 +131,7 @@
     ws.onmessage = function (e) {
         let message = {};
 
-        console.log(e);
+        console.log('message');
         try {
             message = JSON.parse(e.data)
         } catch (e) {
