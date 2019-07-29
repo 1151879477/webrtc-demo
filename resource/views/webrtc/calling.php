e53883f9d5bcdd08569c1d4ea41bcb95b3be80d2
daiyu <1151879477@qq.com>
2019-07-29 13:56:40 +0800

    1

--- resource/views/webrtc/calling.php
+++ resource/views/webrtc/calling.php
@@ -18,7 +18,7 @@
 <body>
 <div class="row" id="messageBox">
     <div class="col-md-12">
-        <div class="alert alert-warning alert-dismissible" role="alert">
+        <div class="alert alert-success alert-dismissible" role="alert">
             <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
             <strong>Warning!</strong> Better check yourself, you're not looking too good.
         </div>
