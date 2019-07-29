4ec369cfcd3fcee2ad93ebf090398df210a96236
daiyu <1151879477@qq.com>
2019-07-29 13:58:48 +0800

    1

--- resource/views/webrtc/calling.php
+++ resource/views/webrtc/calling.php
@@ -24,6 +24,34 @@
             <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
             <strong>没有风筝的线:</strong> <span>：在吗？</span>
         </div>
+        <div class="alert alert-success alert-dismissible" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <strong>没有风筝的线:</strong> <span>：在吗？</span>
+        </div>
+        <div class="alert alert-success alert-dismissible" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <strong>没有风筝的线:</strong> <span>：在吗？</span>
+        </div>
+        <div class="alert alert-success alert-dismissible" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <strong>没有风筝的线:</strong> <span>：在吗？</span>
+        </div>
+        <div class="alert alert-success alert-dismissible" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <strong>没有风筝的线:</strong> <span>：在吗？</span>
+        </div>
+        <div class="alert alert-success alert-dismissible" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <strong>没有风筝的线:</strong> <span>：在吗？</span>
+        </div>
+        <div class="alert alert-success alert-dismissible" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <strong>没有风筝的线:</strong> <span>：在吗？</span>
+        </div>
+        <div class="alert alert-success alert-dismissible" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <strong>没有风筝的线:</strong> <span>：在吗？</span>
+        </div>
     </div>
 </div>
 <div class="row">
